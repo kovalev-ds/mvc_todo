0becc5d03ed37f315118196dd2542f7a470d9121
kovvdn <40830091+kovvdn@users.noreply.github.com>
2021-04-26 16:23:26 +0300

    change remove hashchange filter

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -53,14 +53,14 @@ class Model {
   }
 }
 
-// simple class to create DOM Elements
+// static class to create DOM Elements
 class UI {
-  static config = {
-    markup: "",
-    attrs: {},
-    events: {},
-    children: null,
-  };
+  // static config = {
+  //   markup: "",
+  //   attrs: {},
+  //   events: {},
+  //   children: null,
+  // };
 
   static create(tag, opts) {
     const { attrs, events, markup, children } = Object.assign(
@@ -85,6 +85,8 @@ class UI {
   }
 }
 
+UI.config = { markup: "", attrs: {}, events: {}, children: null };
+
 class View {
   constructor(selector) {
     this.parentNode = document.querySelector(selector) || document.body;
@@ -103,18 +105,24 @@ class View {
         class: "filter",
       },
       children: [
-        { href: "#", title: "All" },
-        { href: "#active", title: "Active" },
-        { href: "#completed", title: "Completed" },
-      ].map(link =>
+        { filter: "all", title: "All" },
+        { filter: "active", title: "Active" },
+        { filter: "completed", title: "Completed" },
+      ].map(item =>
         UI.create("li", {
           children: [
-            UI.create("a", {
+            UI.create("button", {
               attrs: {
-                href: link.href,
-                class: `filter__link`,
+                class: `filter__button`,
+                "data-filter": item.filter,
               },
-              markup: link.title,
+              markup: item.title,
+              events: {
+                click: () => {
+                  this.handleFilterChange(item.filter);
+                  this.updateActiveFilter(item.filter);
+                },
+              },
             }),
           ],
         })
@@ -186,6 +194,12 @@ class View {
     };
   }
 
+  bindFilterChange(cb) {
+    this.handleFilterChange = filter => {
+      cb(filter);
+    };
+  }
+
   bindFormSubmit(cb) {
     this.formUI.addEventListener("submit", event => {
       event.preventDefault();
@@ -198,17 +212,18 @@ class View {
         },
         {}
       );
+
       cb(data);
 
       event.currentTarget.reset();
     });
   }
 
-  updateActiveFilter() {
+  updateActiveFilter(filter) {
     [].forEach.call(this.filtersUI.children, el => {
-      el.children[0].classList.remove("filter__link--active");
-      el.children[0].hash === location.hash &&
-        el.children[0].classList.add("filter__link--active");
+      el.children[0].classList.remove("filter__button--active");
+      el.children[0].dataset.filter === filter &&
+        el.children[0].classList.add("filter__button--active");
     });
   }
 
@@ -295,54 +310,72 @@ class TodoApp {
   constructor(opts) {
     this.view = new View(opts.el);
     this.model = new Model(opts.appID);
+    this.filterMode = "all";
   }
+
+  _filter(filter, item) {
+    if (filter === "active") return !item.completed;
+    if (filter === "completed") return item.completed;
+    return true;
+  }
+
+  onTodoListChanged = todos => {
+    this.view.updateTodoList(
+      todos.filter(item => this._filter(this.filterMode, item))
+    );
+    this.view.updateCounter(todos.filter(item => !item.completed).length);
+  };
+
+  onFilterChanged = filter => {
+    this.filterMode = filter;
+    this.model.getAll(todos => {
+      this.view.updateTodoList(
+        todos.filter(item => this._filter(this.filterMode, item))
+      );
+    });
+  };
+
+  handleAddTask = data => {
+    if (!data.title.trim()) return;
+
+    const task = {
+      id: Math.floor(Math.random() * Date.now()),
+      completed: false,
+      title: data.title.trim(),
+    };
+
+    this.model.add(task);
+  };
+  handleEditTodo = (id, data) => this.model.edit(id, data);
+  handleDeleteTodo = id => this.model.delete(id);
+  handleToggleTodo = id => this.model.toggle(id);
 
   init() {
     this.view.initUI();
-    this.view.bindFormSubmit(data => {
-      const task = {
-        id: Math.floor(Math.random() * Date.now()),
-        completed: false,
-        title: data.title,
-      };
-
-      this.model.add(task);
-    });
-
-    this.view.bindTaskDelete(id => this.model.delete(id));
-    this.view.bindTaskComplete(id => this.model.toggle(id));
-    this.view.bindTaskEdit((id, data) => this.model.edit(id, data));
-
-    this.model.bindTodoListChanged(todos => {
-      this.view.updateTodoList(todos.filter(item => filterByHash(item)));
-      this.view.updateCounter(todos.filter(item => !item.completed).length);
-    });
+    this.view.updateActiveFilter(this.filterMode);
 
     this.model.getAll(todos => {
-      this.view.updateActiveFilter();
-      this.view.updateTodoList(todos.filter(item => filterByHash(item)));
+      this.view.updateTodoList(
+        todos.filter(item => this._filter(this.filterMode, item))
+      );
       this.view.updateCounter(todos.filter(item => !item.completed).length);
     });
 
-    window.addEventListener("hashchange", () => {
-      this.model.getAll(todos => {
-        this.view.updateTodoList(todos.filter(item => filterByHash(item)));
-        this.view.updateActiveFilter();
-      });
-    });
+    this.view.bindFormSubmit(this.handleAddTask);
+    this.view.bindTaskDelete(this.handleDeleteTodo);
+    this.view.bindTaskComplete(this.handleToggleTodo);
+    this.view.bindTaskEdit(this.handleEditTodo);
+    this.view.bindFilterChange(this.onFilterChanged);
+    this.model.bindTodoListChanged(this.onTodoListChanged);
   }
 }
 
-// utils
-
-const filterByHash = item => {
-  const { hash } = location;
-  if (hash === "#active") return !item.completed;
-  if (hash === "#completed") return item.completed;
-  return true;
-};
+new TodoApp({
+  appID: "first",
+  el: "#root",
+}).init();
 
 new TodoApp({
-  appID: "todo",
+  appID: "second",
   el: "#root",
 }).init();
